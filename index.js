add0409a0cc0390a49344658968a91adeca65568
aksuharun <harun16aksu@gmail.com>
2023-06-10 02:18:18 +0300

    Add author command and comment lines

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ const TelegramBot = require('node-telegram-bot-api')
 const token = YOUR_API_KEY
 const bot = new TelegramBot(token,{polling:true})
 
-
+//Define language variables
 const langs = ["en" ,"tr"]
 var activeLang = 0;
+
 const Cmd = class{
 	constructor(help){
 		this.help = help
 	}
 }
 
+// Define random
 const random = new Cmd([
 	"/random command is a random number and letter generator between 0-9 and A-Z. Type \"num\" or \"number\" (non case sensitive) after /random command to generate random number. Type \"letter\" to generate random letter",
 	"/random komutu 0-9 ve A-Z arasında sayı ya da harf üretir. Sayı üretmek için /random yazdıktan sonra \"sayı\" ya da \"sayi\" (büyük/küçük harf olması önemsiz) yazın.\n Harf üretmek içinse \"harf\" yazın"
 ])
 
+//Define lang
 const lang = new Cmd("🇬🇧 Use /lang command to change language. eg: \"/lang en\". Use /help command to see commands\n\n 🇹🇷 Dil Seçmek için /lang komutunu kullan. Örnek: \"/lang tr\"")
+
+// ----- ON TEXT ------ 
+
+// start
 bot.onText(/\/start/,(msg)=>{
 	bot.sendMessage(msg.chat.id,lang.help)
 })
 
+// lang
 bot.onText(/\/lang (.+)/, (msg,match) => {
 	const chatId = msg.chat.id
 	const res = match[1]
@@ -35,6 +43,8 @@ bot.onText(/\/lang (.+)/, (msg,match) => {
 		bot.sendMessage(chatId,"🇹🇷 Dil Türkçe olarak ayarlandı")
 	}
 }) 
+
+// echo
 bot.onText(/\/echo (.+)/,(msg,match) => {
 	const chatId = msg.chat.id
 	const res = match[1]
@@ -42,6 +52,8 @@ bot.onText(/\/echo (.+)/,(msg,match) => {
 	bot.sendMessage(chatId,res)
 })
 
+
+// random
 bot.onText(/\/random (.+)/,(msg,match) =>{
 	const chatId = msg.chat.id
 	const res =  match[1].toLowerCase()
@@ -58,6 +70,8 @@ bot.onText(/\/random (.+)/,(msg,match) =>{
 	}
 })
 
-bot.onText(/\/author/,(msg,match)=>{
-	bot.sendMessage(msg.chat.id,)
+
+// author
+bot.onText(/\/author/,(msg) => {
+	bot.sendMessage(msg.chat.id,"Author is<a href='github.com/aksuharun'>Harun Aksu</a>",{parse_mode:'HTML'})
 })
